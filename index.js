b212a97042dfe000055d2fda8ece861d1056d3fb
drftgyhuuy6 <gs1idmyname@hotmail.com>
2024-05-14 10:21:28 +0700

    start project commit

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-function countBits(num) {
-  const result = new Array(num + 1).fill(0);
-  for (let i = 1; i <= num; i++) {
-    result[i] = result[i >> 1] + (i & 1);
+function isValidBST(root) {
+  return isValid(root, null, null);
+  function isValid(node, min, max) {
+    if (!node) return true;
+    if ((min !== null && node.val <= min) || (max !== null && node.val >= max))
+      return false;
+    return (
+      isValid(node.left, min, node.val) && isValid(node.right, node.val, max)
+    );
   }
-  return result;
 }
